e90f3fb7750717ca416802866bc7c96053dc6632
Sundeep Dhall <sundeepdhall@yahoo.com>
2018-02-10 08:30:34 -0600

    Changed message

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-	return "Hello my dear sundeep Moto !!"
+	return "Hello sundeep Moto !!"
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True, port=80)
