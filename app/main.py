edf867d271fc8100dfd841f05487f7718b4cf5db
Sundeep Dhall <sundeepdhall@yahoo.com>
2018-02-10 08:43:22 -0600

    A test change "This is a test"

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-	return "Hello sundeep Moto !!"
+	return "Hello This is a test sundeep Moto !!"
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True, port=80)
