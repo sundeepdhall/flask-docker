37e22685a93e2a2a94f881e80ad4c4d04e16e85a
Sundeep Dhall <sundeepdhall@yahoo.com>
2018-04-18 09:36:33 -0500

    demo with IBM team

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-	return "Hey Shawn Kelley, we are using Docker.  a 2nd run!!"
+	return "Hey IBM team, we are using Docker and Eclipse!!"
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True, port=8000)
