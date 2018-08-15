b43370c159880f6af793c7dd6fbcf3dba06a48d6
Sundeep Dhall <sundeepdhall@yahoo.com>
2018-08-15 14:44:56 -0400

    # WARNING: head commit changed in the meantime
    Test

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello team, we are testing using Docker for development"
+    return "Hello team NCR, we are testing using Docker for development"
 
 
 if __name__ == "__main__":
